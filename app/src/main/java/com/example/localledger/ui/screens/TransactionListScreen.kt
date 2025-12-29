11c6d23705b173c04168d2e76538e73c3660e776
Wang Hongyuan <72510222@cityu-dg.edu.cn>
2025-12-29 21:46:46 +0800

    Update TransactionListScreen.kt

--- app/src/main/java/com/example/localledger/ui/screens/TransactionListScreen.kt
+++ app/src/main/java/com/example/localledger/ui/screens/TransactionListScreen.kt
@@ -1,22 +1,22 @@
 package com.example.localledger.ui.screens
 
 import androidx.compose.animation.animateColorAsState
-import androidx. compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation. background
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation. lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx. compose.material.icons.filled. Delete
+import androidx.compose.material.icons.filled. Delete
 import androidx.compose.material3.*
-import androidx.compose.runtime. Composable
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx. compose.ui.Alignment
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose. ui.text.style.TextAlign
-import androidx.compose.ui. unit.dp
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.localledger.data.model.TransactionEntity
 import java.text.SimpleDateFormat
 import java.util.*
